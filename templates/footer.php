2b73d4b03781eaba3316ed0a80bb1c3e6a245f98
WevertonOtoni <weverton.otoni@live.com>
2022-07-28 12:00:51 -0300

    separação de templates

---
+++ templates/footer.php
@@ -0,0 +1,34 @@
+<footer id="footer">
+    <div id="social-container">
+        <ul>
+            <li>
+                <a href=""><i class="fab fa-facebook-square"></i></a>
+            </li>
+            <li>
+                <a href=""><i class="fab fa-instagram"></i></a>
+            </li>
+            <li>
+                <a href=""><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+    </div>
+    <div id="footer-links-container">
+        <ul>
+            <li>
+                <a href="">Adicionar filme</a>
+            </li>
+            <li>
+                <a href="">Adicionar crítica</i></a>
+            </li>
+            <li>
+                <a href="">Entrar / Cadastrar</a>
+            </li>
+        </ul>
+    </div>
+    <p>&copy; 2022 Weverton Otoni</p>
+</footer>
+<!-- Bootstrap JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.js" integrity="sha512-39Q5Yw4jtWPVkYu5Dry3HiRh6DWG4FDODb/rHF+X2Xp0kzhg9VgTyYV209uQ/EsqPDP/4dDvwm1rb8JgnAkHNg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+</html>
